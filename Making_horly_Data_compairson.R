61aa97a20541b1e0a587f7ca3d40c1edf87dbd36
PhilippAusgust <s7phmuen@uni-bonn.de>
2022-11-08 23:00:17 +0100

    add fixes

--- Making_horly_Data_compairson.R
+++ Making_horly_Data_compairson.R
@@ -31,14 +31,9 @@ final
 
 #NAs generieren
 
+zuelpich_april$Tmax_Tmin = NA
 
-zuelpich_april$max
 
-for(i in nrow(zuelpich_april[,2])){
-  zuelpich_april$max = NA
-
-}
-zuelpich_april$max
 
 
 # Indizies generieren 
@@ -47,7 +42,7 @@ zuelpich_april$max
 for (i in seq(1, nrow(zuelpich_april))) {
   for (j in seq(1, nrow(final))) {
     if (zuelpich_april$temperature[i] == final$Tmax[j]) {
-      zuelpich_april$max[i] <- final$Tmax[j]
+      zuelpich_april$Tmax_Tmin[i] <- final$Tmax[j]
     }
   }
 }
@@ -57,67 +52,26 @@ for (i in seq(1, nrow(zuelpich_april))) {
 for(i in seq(1, nrow(zuelpich_april))) {
   for (j in seq(1, nrow(final))) {
     if (zuelpich_april$temperature[i] == final$Tmin[j]) {
-      zuelpich_april$max[i] <- final$Tmin[j]
+      zuelpich_april$Tmax_Tmin[i] <- final$Tmin[j]
     }
   }
 }
 
 
-
-
-zuelpich_april
-
-
-
-
-
-
-
-ggplot(data=zuelpich_april, aes(x= zuelpich_april[,4], y = zuelpich_april[,2]))+
-  geom_line(size= 1.0, colour = "darkgreen")+
-  labs(x = "Date", y = "Temperature (C°)")+
-  ggtitle("1 to 5 April 2019 weather station Zuelpich")+theme_bw(base_size = 20)
+ggplot(data = zuelpich_april, aes(x = zuelpich_april[, 4], y = zuelpich_april[, 2])) +
+  geom_line(size = 1.0, colour = "darkgreen") +
+  geom_point(aes(y = zuelpich_april$Tmax_Tmin),
+             colour = "red",
+             size = 3.0) +
+  labs(x = "Date", y = "Temperature (C°)") +
+  ggtitle("1 to 20 April 2019 weather station Zuelpich") +
+  theme_bw(base_size = 20)
 
 
 
 
 
 
-require(chillR)
-require(ggplot2)
-require(reshape2)
-require(kableExtra)
-require(ggplot2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-KA_hours<-KA_weather[10:20,]
-KA_hours[,"Hour"]<-0
-KA_hours$Hour[nrow(KA_hours)]<-23
-
-KA_hours[,"Temp"]<-0
-KA_hours<-make_all_day_table(KA_hours,timestep="hour")
-KA_hours[nrow(KA_hours),]
-
-for(i in 2:nrow(KA_hours))
-{
-  if (is.na(KA_hours$Tmin[i]))
-    KA_hours$Tmin[i] <- KA_hours$Tmin[i - 1]
-  if (is.na(KA_hours$Tmax[i]))
-    KA_hours$Tmax[i] <- KA_hours$Tmax[i - 1]
-}
 
 
 
